45cdd337d8f90d56babc4813092b25e3cdfe1d87
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2021-08-12 23:29:13 +1000

    ci: Switching to Nuke and semantic-release

---
+++ build/Build.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GlobExpressions;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Coverlet;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.ReportGenerator;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.ReportGenerator.ReportGeneratorTasks;
+
+[CheckBuildProjectConfigurations]
+[ShutdownDotNetAfterServerBuild]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main() => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter("Version of the NuGet package to build")]
+    readonly string Version = string.Empty;
+
+    [Parameter("The API key for publishing NuGet packages")]
+    readonly string NuGetApiKey = string.Empty;
+
+    [Parameter("The access token for making calls to the GitHub API")]
+    readonly string GitHubAccessToken = string.Empty;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath TestDirectory => RootDirectory / "test";
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    AbsolutePath NugetDirectory => ArtifactsDirectory / "nuget";
+    AbsolutePath TestResultDirectory => ArtifactsDirectory / "test-results";
+    AbsolutePath CoverageResultDirectory => ArtifactsDirectory / "coverage";
+    AbsolutePath CoverageReportDirectory => ArtifactsDirectory / "coverage-report";
+
+    IEnumerable<Project> TestProjects => Solution.GetProjects("*.Tests");
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            TestDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(ArtifactsDirectory);
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
+        });
+
+    Target Test => _ => _
+        .DependsOn(Clean, Compile)
+        .Executes(() => DotNetTest(s => s
+            .SetProjectFile(Solution)
+            .SetConfiguration(Configuration)
+            .EnableNoRestore()
+            .EnableNoBuild()
+            .ResetVerbosity()
+            .SetResultsDirectory(TestResultDirectory)
+            .EnableCollectCoverage()
+            .SetCoverletOutputFormat(CoverletOutputFormat.cobertura)
+            .SetExcludeByFile("*.Generated.cs")
+            .CombineWith(TestProjects, (_, v) => _
+                .SetProjectFile(v)
+                .SetLoggers($"trx;LogFileName={v.Name}.trx;FailureBodyFormat=Verbose")
+                .SetCoverletOutput($"{CoverageResultDirectory}/{v.Name}.xml"))));
+
+    Target Cover => _ => _
+        .DependsOn(Test)
+        .Consumes(Test)
+        .Produces(CoverageResultDirectory / "lcov.info")
+        .Executes(() =>
+        {
+            ReportGenerator(_ => _
+                .SetFramework("net5.0")
+                .SetReports(CoverageResultDirectory / "*.xml")
+                .SetTargetDirectory(CoverageReportDirectory)
+                .SetReportTypes((ReportTypes)"lcov")
+                .When(IsLocalBuild, _ => _
+                    .AddReportTypes(ReportTypes.HtmlInline)));
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Clean, Restore)
+        .Requires(() => !string.IsNullOrWhiteSpace(Version))
+        .Executes(() => DotNetPack(s => s
+            .SetConfiguration(Configuration.Release)
+            .SetVersion(Version)
+            .SetOutputDirectory(NugetDirectory)
+            .SetProject(Solution)));
+
+    Target Publish => _ => _
+        .DependsOn(Pack)
+        .Executes(() => { });
+
+    Target PublishGitHub => _ => _
+        .OnlyWhenStatic(() => IsServerBuild)
+        .Requires(() => !string.IsNullOrWhiteSpace(NuGetApiKey))
+        .TriggeredBy(Publish)
+        .Executes(() => DotNetNuGetPush(s => s
+            .SetSource($"https://nuget.pkg.github.com/{GitRepository.Identifier.Split('/').First()}/index.json")
+            .SetApiKey(GitHubAccessToken)
+            .CombineWith(Glob.Files(NugetDirectory, "*.nupkg"), (ss, package) => ss
+                .SetTargetPath(NugetDirectory / package))));
+
+    Target PublishNuGet => _ => _
+        .OnlyWhenStatic(() => IsServerBuild && GitRepository.IsOnMainOrMasterBranch())
+        .Requires(() => !string.IsNullOrWhiteSpace(NuGetApiKey))
+        .TriggeredBy(Publish)
+        .Executes(() => DotNetNuGetPush(s => s
+            .SetSource("https://api.nuget.org/v3/index.json")
+            .SetApiKey(NuGetApiKey)
+            .CombineWith(Glob.Files(NugetDirectory, "*.nupkg"), (ss, package) => ss
+                .SetTargetPath(NugetDirectory / package))));
+}
