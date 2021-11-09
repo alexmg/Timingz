b722107883b8c42b48e67fca5ea311595d6692e5
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2021-11-09 23:45:38 +1000

    build: Upgrade projects to .NET 6
    build: Upgrade NuGet packages

--- samples/WebApiSample/Controllers/SampleController.cs
+++ samples/WebApiSample/Controllers/SampleController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Timingz;
 
--- samples/WebApiSample/Program.cs
+++ samples/WebApiSample/Program.cs
@@ -1,23 +1,96 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using OpenTelemetry.Instrumentation.AspNetCore;
 using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Timingz;
+using WebApiSample;
+using WebApiSample.Services;
 
-namespace WebApiSample
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Add the required services to the dependency injection container.
+// The IServerTiming service is scoped the HTTP request and is available for dependency injection.
+builder.Services.AddServerTiming();
+
+// Callback services can be registered that will be invoked after the request is completed.
+// These can have a lifetime that is scoped to the request or a singleton.
+// The event received will contain the HttpContext and a list of metrics that were recorded.
+// You can use this to send the collected metrics to another service or metrics package.
+builder.Services.AddScoped<IServerTimingCallback, SampleServerTimingCallback>();
+
+// Configure OpenTelemetry tracing to see our custom Activity
+// being exported and included in the Server-Timing header.
+builder.Services.AddOpenTelemetryTracing(tpb => tpb
+    .SetResourceBuilder(ResourceBuilder
+        .CreateDefault()
+        .AddService(builder.Environment.ApplicationName))
+    .AddSource(Telemetry.Source.Name)
+    .AddAspNetCoreInstrumentation()
+    .AddHttpClientInstrumentation()
+    .AddConsoleExporter());
+
+builder.Services.Configure<AspNetCoreInstrumentationOptions>(options =>
+    options.RecordException = true);
+
+builder.Host.ConfigureLogging(builder => builder.AddOpenTelemetry(options =>
 {
-    public static class Program
-    {
-        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+    options.IncludeScopes = true;
+    options.ParseStateValues = true;
+    options.IncludeFormattedMessage = true;
+    options.AddConsoleExporter();
+}));
 
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
-                .ConfigureLogging(builder => builder.AddOpenTelemetry(options =>
-                {
-                    options.IncludeScopes = true;
-                    options.ParseStateValues = true;
-                    options.IncludeFormattedMessage = true;
-                    options.AddConsoleExporter();
-                }));
-    }
-}
+var app = builder.Build();
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+// Add the middleware before UseEndpoints.
+app.UseServerTiming(options =>
+{
+    // Use the callback below to configure the per-request options. You can use the provided HttpContext to tailor
+    // the options for individual requests or statically define the options and have them apply to all requests.
+    options.WithRequestTimingOptions((httpContext, requestOptions) =>
+    {
+        // The sample uses query string parameters to provide an easy way of toggling the settings
+        // and seeing the resulting change in behaviour. Replace these with something more appropriate
+        // for a production quality application.
+        var query = httpContext.Request.Query;
+
+        // Server-Timing headers could expose sensitive information and must be explicitly enabled.
+        // The HttpContext is available in this callback so you can configure this per request if required.
+        requestOptions.WriteHeader = query.ContainsKey("timing");
+
+        // The metric descriptions increase the Server-Timing header size and must be explicitly enabled.
+        // The HttpContext is available in this callback so you can configure this per request if required.
+        requestOptions.IncludeDescriptions = query.ContainsKey("desc");
+
+        // Custom metrics could expose sensitive information in the Server-Timing header and must be explicitly enabled.
+        // You can choose to include additional metrics (other than the request total duration) conditionally.
+        requestOptions.IncludeCustomMetrics = query.ContainsKey("custom");
+    });
+
+    // Add any required origins for the Timing-Allow-Origin header.
+    options.TimingAllowOrigins = new[] { "http://example.com" };
+
+    // You may want to invoke the callbacks with the timings to record them elsewhere,
+    // even if you do not want to send the Server-Timing header in the response.
+    // This option also allows you to register callback services unconditionally and only
+    // have them invoked when required.
+    options.InvokeCallbackServices = true;
+
+    // When this setting is enabled the middleware will throw an exception if a metric has not been used correctly.
+    // For example, a manual metric was started but not stopped, or a disposable metric was not disposed.
+    // This is intended to help you find incorrect logic in your code and prevent invalid metrics from being sent.
+    // You may choose to enable this feature only in a local or development environment.
+    // If the request has a status code of 500 the metrics will not be validated to avoid further exceptions.
+    options.ValidateMetrics = app.Environment.IsDevelopment();
+
+    // Add the Activity Source that should be monitored for AddServerTiming calls.
+    options.ActivitySources.Add(Telemetry.Source.Name);
+});
+
+app.Run();
