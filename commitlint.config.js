cfeed22d47269d0abd5ce0ddc1674c19adfc8dd5
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2021-11-20 01:40:18 +1000

    style: Fix string quote style in JS file

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
+    extends: ["@commitlint/config-conventional"],
 
     rules: {
-        'type-enum':
-            [2, 'always', [
-                'revert', 'feat', 'fix', 'perf', 'docs', 'test', 'build', 'refactor', 'style', 'chore'
+        "type-enum":
+            [2, "always", [
+                "revert", "feat", "fix", "perf", "docs", "test", "build", "refactor", "style", "chore"
             ]],
 
         // Default rules we want to suppress:
-        'body-leading-blank': [0, "always"],
-        'body-max-line-length': [0, "always"],
-        'footer-max-line-length': [0, "always"],
-        'footer-leading-blank': [0, "always"],
-        'subject-case': [0, "always", []],
-        'subject-full-stop': [0, "never", '.']
+        "body-leading-blank": [0, "always"],
+        "body-max-line-length": [0, "always"],
+        "footer-max-line-length": [0, "always"],
+        "footer-leading-blank": [0, "always"],
+        "subject-case": [0, "always", []],
+        "subject-full-stop": [0, "never", "."]
     }
 };
